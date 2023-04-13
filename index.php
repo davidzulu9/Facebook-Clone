55231292fc29678117af08fa2e05cdb971fe6b0d
Volk <Volk@DESKTOP-G1QCN88>
2023-04-13 17:05:52 +0200

    Add project files.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Web</title>
+    </head>
+    <body>
+        <div>
+            <h1>facebook</h1>
+            Facebook helps you connect with your friends
+        </div>
+        <div>
+            <input /> 
+            <input />
+            <button>Log in</button>
+            <a>Forgot password</a>
+        </div>
+    </body>
+</html>
